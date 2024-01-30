43b41f1d1a96db088079cc68ab76f233da24514e
limstonestone <ehus1118@gmail.com>
2024-01-30 18:41:00 +0900

    [240125] 토마토(7569)

---
+++ boj/Gold/Gold5/[240125] 토마토(7569).py
@@ -0,0 +1,74 @@
+"""
+https://www.acmicpc.net/problem/7569
+"""
+
+
+def try1():
+    """
+    - 약 23분 소요
+    - 그래프 탐색 + 최단거리 -> BFS
+        - 이때, 그래프가 3차원임을 유의
+        - 따라서 원하는 좌표를 찾으려면 z, y, x (x,y,z 역순) 순으로 인덱싱
+    - 데이터 정의만 잘 해준다면 2차원 BFS 와 다를 것 없음
+    """
+    import sys
+    from collections import deque
+
+    input = sys.stdin.readline
+
+    m, n, h = map(int, input().split())
+
+    graph = [[] for _ in range(h)]
+    tomato = []
+
+    for z in range(h - 1, -1, -1):
+        for y in range(n):
+            tmp = list(map(int, input().split()))
+            ls = []
+            for x in range(m):
+                ls.append(tmp[x])
+                if tmp[x] == 1:
+                    tomato.append((z, y, x))
+            graph[z].append(ls)
+
+    def bfs(graph, tomato):
+        q = deque(tomato)
+        direction = [
+            (1, 0, 0),
+            (-1, 0, 0),
+            (0, 1, 0),
+            (0, -1, 0),
+            (0, 0, 1),
+            (0, 0, -1),
+        ]
+
+        while q:
+            z, y, x = q.popleft()
+
+            for dx, dy, dz in direction:
+                nx, ny, nz = x + dx, y + dy, z + dz
+
+                if (0 <= nx < m) & (0 <= ny < n) & (0 <= nz < h):
+                    if graph[nz][ny][nx] == 0:
+                        graph[nz][ny][nx] += graph[z][y][x] + 1
+                        q.append((nz, ny, nx))
+
+        return graph
+
+    graph = bfs(graph, tomato)
+    ans = 0
+
+    for z in range(h):
+        for y in range(n):
+            for x in range(m):
+                if graph[z][y][x] == 0:  # 익지 못한 토마토가 존재한다면 바로 실패
+                    print(-1)
+                    exit()
+                elif graph[z][y][x] != -1:
+                    ans = max(ans, graph[z][y][x])
+
+    print(ans - 1)
+
+
+if __name__ == "__main__":
+    try1()
