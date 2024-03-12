80ab72e0f18ef2f8ba2f301c9872b79fcdcb8884
limstonestone <ehus1118@gmail.com>
2024-03-12 14:19:49 +0900

    [240312] 포도주 시식

--- boj/Silver/Silver1/[240117] 포도주 시식.py
+++ boj/Silver/Silver1/[240117] 포도주 시식.py
@@ -30,5 +30,32 @@ def try1():
     print(dp[n - 1])
 
 
+def try2():
+    """
+    - 약 35분 소요
+    - 과거의 연산 값을 통해서 현재의 최선 값을 찾아내기 -> DP
+    - 발생할 사건의 분할을 생각해보면 점화식 도출 가능, 현재 시점의 최대값은 아래와 같음
+        - 현재 와인을 고르지 않기 (dp[i-1])
+        - 현재 와인을 고르되 이전 와인은 고르지 않음 (dp[i-2] + wine[i])
+        - 현재 와인을 고르되 이전 와인도 고름 (dp[i-3] + wine[i-1] + wine[i])
+    """
+    import sys
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    wine = [int(input()) for _ in range(n)] + ([0] * 2)
+    dp = [0] * (n + 2)
+    dp[0] = wine[0]
+    dp[1] = wine[0] + wine[1]
+    dp[2] = max(dp[1], wine[1] + wine[2], wine[0] + wine[2])
+
+    for i in range(3, n):
+        dp[i] = max(dp[i - 1], dp[i - 3] + wine[i - 1] + wine[i], dp[i - 2] + wine[i])
+
+    print(dp[n - 1])
+
+
 if __name__ == "__main__":
-    try1()
+    # try1()
+    try2()
