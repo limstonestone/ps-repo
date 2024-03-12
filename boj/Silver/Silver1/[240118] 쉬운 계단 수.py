f684418fab4f063cbe08709be53f957285475aeb
limstonestone <ehus1118@gmail.com>
2024-03-12 15:17:42 +0900

    [240312] 쉬운 계단 수

--- boj/Silver/Silver1/[240118] 쉬운 계단 수.py
+++ boj/Silver/Silver1/[240118] 쉬운 계단 수.py
@@ -38,5 +38,33 @@ def try1():
     print(sum(dp[n]) % 1_000_000_000)
 
 
+def try2():
+    """
+    - 약 45분 소요
+    - 계단 수는 결국 앞자리 수의 영향을 받음
+        - 앞자리의 경우의 수 + 2
+    - 이때 예외적으로 현재 자리가 9라면 앞에 8밖에 오지 못함
+    - 또한 현재 자리가 0이라면 앞자리에 1밖에 오지 못함
+    - 0과 9가 고정이므로 2차원 배열의 인덱스를 통해 가지를 뻗어나가는 식으로 구현
+    """
+    import sys
+
+    input = sys.stdin.readline
+    n = int(input())
+    dp = [[0 for _ in range(10)] for _ in range(n + 1)]
+
+    for j in range(1, 10):
+        dp[1][j] += 1
+
+    for i in range(2, n + 1):
+        for j in range(1, 9):
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+        dp[i][0] = dp[i - 1][1]
+        dp[i][9] = dp[i - 1][8]
+
+    print(sum(dp[n]) % 1_000_000_000)
+
+
 if __name__ == "__main__":
-    try1()
+    # try1()
+    try2()
