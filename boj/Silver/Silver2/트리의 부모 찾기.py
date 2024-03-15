5287a5039242538576d012d5fd1d2888655edeb9
limstonestone <ehus1118@gmail.com>
2024-03-15 22:20:00 +0900

    [240315] 트리의 부모 찾기

--- boj/Silver/Silver2/트리의 부모 찾기.py
+++ boj/Silver/Silver2/트리의 부모 찾기.py
@@ -38,13 +38,41 @@ def try1():
         print(visited[i])
 
 
-if __name__ == "__main__":
-    try1()
+def try2():
+    """
+    - 약 15분 소요
+    - DFS 를 활용해 자식노드를 탐색
+    - 자식노드를 방문했을 때 이전 부모노드의 값을 딕셔너리에 저장
+    """
+    import sys
 
-"""
-1 - 6 - 3 - 5
-|
-4 - 2
-|
-7
-"""
+    sys.setrecursionlimit(987654321)
+
+    input = sys.stdin.readline
+    n = int(input())
+    graph = [[] for _ in range(n + 1)]
+
+    for _ in range(n - 1):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    answer = {}
+    visited = [False] * (n + 1)
+
+    def dfs(x):
+        visited[x] = True
+
+        for i in graph[x]:  # i는 자식노드
+            if not visited[i]:
+                answer[i] = x  # 부모노드 저장
+                dfs(i)
+
+    dfs(1)  # 루트 노드부터 탐색 시작
+    for i in range(2, n + 1):
+        print(answer[i])
+
+
+if __name__ == "__main__":
+    # try1()
+    try2()
