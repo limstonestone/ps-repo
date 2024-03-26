2e04a7af181a750873fbd8ecb34e4dcd7bb34ae1
limstonestone <ehus1118@gmail.com>
2024-03-26 17:17:09 +0900

    [240326] 업그레이드 할 수 없는 아이템 구하기

---
+++ SQL/programmers/Level3/업그레이드 할 수 없는 아이템 구하기.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/273712
+
+select item_id, item_name, rarity
+from item_info
+where item_id not in (select distinct parent_item_id 
+                      from item_tree 
+                      where parent_item_id is not null)
+order by item_id desc
+;
