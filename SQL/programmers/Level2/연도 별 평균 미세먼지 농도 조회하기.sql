7346c4ca41e5187e9e06b60ea402465384820842
limstonestone <ehus1118@gmail.com>
2024-03-26 17:26:30 +0900

    [240326] 연도 별 평균 미세먼지 농도 조회하기

---
+++ SQL/programmers/Level2/연도 별 평균 미세먼지 농도 조회하기.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/284530
+
+select year(ym) as year, round(avg(pm_val1),2) as "pm10", 
+    round(avg(pm_val2),2) as "pm2.5"
+from air_pollution
+where location2 = "수원"
+group by year
+order by year
+;
