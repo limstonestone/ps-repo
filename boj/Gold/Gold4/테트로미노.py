0eca666393165a881283fdb2372c9fa048891aba
limstonestone <ehus1118@gmail.com>
2024-03-29 17:05:51 +0900

    [240329] 테트로미노

--- boj/Gold/Gold4/테트로미노.py
+++ boj/Gold/Gold4/테트로미노.py
@@ -68,5 +68,73 @@ def try1():
     print(ans)
 
 
+def try2():
+    """
+    - 약 50분 풀이
+    - 기준점을 잡아 테트로미노의 회전과 대칭을 고려해야함이 핵심
+    - 그래프탐색 알고리즘을 사용할 필요는 없음
+    - 회전과 대칭을 고려하기가 조금 복잡할 수 있지만, 회전4 * 대칭2 = 8
+        - 이를 고려해보면 아래 transform 함수를 쉽게 도출 가능
+    """
+
+    import sys
+
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+
+    graph = [list(map(int, input().split())) for _ in range(n)]
+
+    tet1 = [(1, 0), (1, 0), (1, 0)]  # ㅡ 모양
+    tet2 = [(1, 0), (0, -1), (-1, 0)]  # ㅁ 모양
+    tet3 = [(0, -1), (0, -1), (1, 0)]  # L 모양
+    tet4 = [(1, 0), (0, -1), (1, 1)]  # ㅜ 모양
+    tet5 = [(0, -1), (1, 0), (0, -1)]  # 남은 모양
+
+    def transform(tet_lst):  # 90도 회전 4번 * 각각 대칭
+        transformed_tet = []
+        for tet in tet_lst:
+            tmp_tet = []
+            tmp_tet.append([(x, y) for x, y in tet])
+            tmp_tet.append([(-x, y) for x, y in tet])
+            tmp_tet.append([(x, -y) for x, y in tet])
+            tmp_tet.append([(-x, -y) for x, y in tet])
+            tmp_tet.append([(y, x) for x, y in tet])
+            tmp_tet.append([(-y, x) for x, y in tet])
+            tmp_tet.append([(y, -x) for x, y in tet])
+            tmp_tet.append([(-y, -x) for x, y in tet])
+            transformed_tet.append(tmp_tet)
+
+        return transformed_tet
+
+    tet1, tet2, tet3, tet4, tet5 = transform([tet1, tet2, tet3, tet4, tet5])
+
+    def search(x, y):
+        ans = 0
+        for total_tet in (tet1, tet2, tet3, tet4, tet5):  # 8개의 변환된 테트로미노 추출
+            for tet in total_tet:  # 각 테트로미노 확인
+                tmp_ans = graph[x][y]  # 현재 좌표값도 포함
+                nx, ny = x, y
+                for dx, dy in tet:
+                    nx += dx
+                    ny += dy
+                    if 0 <= nx < n and 0 <= ny < m:
+                        tmp_ans += graph[nx][ny]
+                    else:  # 중간에 테트로미노가 범위를 벗어난다면 실패 처리
+                        tmp_ans = 0
+                        break
+                ans = max(ans, tmp_ans)
+
+        return ans
+
+    ans = 0
+    for x in range(n):
+        for y in range(m):
+            ans = max(ans, search(x, y))
+
+    print(ans)
+
+
 if __name__ == "__main__":
-    try1()
+    # try1()
+    try2()
